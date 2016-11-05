c2cc21b273eb40541fa1b927d396fa0be9e9b2a0
dj-m <domingomoronta@gmail.com>
2016-11-05 09:42:29 -0700

    add treenode class

--- trees.rb
+++ trees.rb
@@ -1,1 +1,22 @@
+=begin
+Creating a Tree Node class where each node contains a value.
+There are pointers to two children, a left and a right.
+=end
+
 class TreeNode
+  attr_accessor :left, :right, :value
+
+  def initialize val, left, right
+    @value = val
+    @left = left
+    @right = right
+  end
+
+  def print
+    print @value + " "
+  end
+
+  def debug
+    print " " + @left.data "-" + @value + "-" + @right.data
+  end
+end
