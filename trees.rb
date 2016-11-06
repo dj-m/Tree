effc00ca7d675f4166e1e6134924eabbb3eae9d4
dj-m <domingomoronta@gmail.com>
2016-11-06 00:13:12 -0700

    refactor BinaryTree class

--- trees.rb
+++ trees.rb
@@ -1,12 +1,9 @@
-=begin
-Creating a Tree Node class where each node contains a value.
-There are pointers to two children, a left and a right.
-=end
-
+# Creating a Tree Node class where each node contains a value.
+# There are pointers to two children, a left and a right.
 class TreeNode
   attr_accessor :value, :left, :right
 
-  def initialize val
+  def initialize(val)
     @value = val
     @left = nil
     @right = nil
@@ -17,19 +14,18 @@ class TreeNode
   end
 
   def debug
-    print "\n #{@value} \n"
-    print " /\\ \n"
-    print "#{@left.value.to_s} #{@right.value.to_s} \n"
+    print "\n#{@value}\n"
+    print " /\\\n"
+    print "#{@left.value} #{@right.value}\n"
   end
 end
 
 # Test values
-root = TreeNode.new("80")
-root.left = TreeNode.new("59")
-root.right = TreeNode.new("95")
-root.left.left = TreeNode.new("19")
-root.left.right = TreeNode.new("78")
-
+# root = TreeNode.new('80')
+# root.left = TreeNode.new('59')
+# root.right = TreeNode.new('95')
+# root.left.left = TreeNode.new('19')
+# root.left.right = TreeNode.new('78')
 
 # Printing values
 # root.puts
@@ -38,40 +34,38 @@ root.left.right = TreeNode.new("78")
 # root.left.right.puts
 # root.right.puts
 
-#Printing one parent and children association
+# Printing one parent and children association
 # root.debug
 # root.left.debug
 
+# Creating a Binary Tree class where each node contains a value.
 class BinaryTree
-  
-  # def in_order
-  # end
-  #
-  # def pre_order
-  # end
-  #
-  # def post_order
-  # end
+  attr_accessor :root
+
+  def in_order
+  end
+
+  def pre_order
+  end
+
+  def post_order
+  end
 
   def create_sample_tree
-    @root = TreeNode.new("Les")
-    @root.left = TreeNode.new("Cathy")
-    @root.left.left = TreeNode.new("Alex")
-    @root.left.right = TreeNode.new("Frank")
-    @root.right = TreeNode.new("Sam")
-    @root.right.left = TreeNode.new("Nancy")
-    @root.right.right = TreeNode.new("Violet")
-    @root.right.right.left = TreeNode.new("Tony")
-    @root.right.right.right = TreeNode.new("Wendy")
+    @root = TreeNode.new('Les'), @root.left = TreeNode.new('Cathy')
+    @root.left.left = TreeNode.new('Alex')
+    @root.left.right = TreeNode.new('Frank')
+    @root.right = TreeNode.new('Sam')
+    @root.right.left = TreeNode.new('Nancy')
+    @root.right.right = TreeNode.new('Violet')
+    @root.right.right.left = TreeNode.new('Tony')
+    @root.right.right.right = TreeNode.new('Wendy')
   end
 
   def puts
-    print "The value at this node is #{@root.value.to_s} \n"
+    print "The value at this node is #{@root.value}\n"
   end
-
 end
 
 t = BinaryTree.new
 t.create_sample_tree
-t.in_order
-t.
